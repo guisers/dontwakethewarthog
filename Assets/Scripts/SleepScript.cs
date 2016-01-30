4a1eedc33c2d40b73204a4e5fe77db57eda5e7b5
Crystal To <crystal.kyto@gmail.com>
2016-01-29 23:25:02 -0800

    basic background and narwhal movement

---
+++ Assets/Scripts/SleepScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+//using System.Collections;
+using System;
+
+public class SleepScript : MonoBehaviour {
+	
+	public double sleep;
+	private double counter = 0;
+
+	void Start () {
+
+	}
+	void Update () {
+		
+	}
+
+	void FixedUpdate() {
+		counter++;
+		sleep = 2 * Math.Sin(0.001 * counter) + 2;
+	}
+}
